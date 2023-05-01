45a4e855acc7f4a0cab4efab58ef55b0e37d4f47
shikharaa <shikhara1997@gmail.com>
2023-05-01 19:48:11 -0400

    m

--- core/lib.c
+++ core/lib.c
@@ -1,47 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include <stddef.h>
 #include "struct-device.h"
 #include "config.h"
 #include "devices.h"
 
-void init_internal(bool);
-char* get_internal();
 
 void config(struct device *alpha)
-{   
+{
     /* User assignments */
     alpha->id = temp;
+
     #ifdef MICROCONTROLLER
         alpha->ssid_wifi = ssid_WiFi;
         alpha->pass_wifi = pass_WiFi;
     #endif
+
     alpha->isEnable[0] = isEnable_VoltageIntern;
     alpha->isEnable[1] = isEnable_TemperatureExtern;
     alpha->isEnable[2] = isEnable_Humidity;
     alpha->isEnable[3] = isEnable_Pressure;
-    alpha->s_name[0] = "InternalVoltage"; 
+
+    alpha->s_name[0] = "InternalVoltage";
     alpha->s_name[1] = "Temperature";
     alpha->s_name[2] = "Humidity";
     alpha->s_name[3] = "Pressure";
+
+
     alpha->interv = intv;
+
 }
 
-void initPeripherals(long* counter) 
+void initPeripherals(long* counter)
 {
-    *counter = 0;
+    *counter = 0;		// Init counter
+
     #ifdef SHELLPRINT
-    	welcome_msg();
+    	welcome_msg();	// Printf in shell
     #endif
+
     init_LEDs();
+    // init_i2c(); // Not necesary in BBB
+
 	init_internal(true);
     init_bme280(true);
 }
 
 
 void led_blinks(int pin, int iterate, int unit_sec)	// LED Blink function-> led: 0 Green LED, 1 Red LED - iter: iterations quantity - usec: delay time in usec
-{	
+{
      int i;
      for (i=0;i<iterate;i++)
      {
@@ -52,32 +59,11 @@ void led_blinks(int pin, int iterate, int unit_sec)	// LED Blink function-> led:
      }
 }
 
-/*void connectNetwork(struct device *alpha, bool first_t)
-{	
-	#ifdef MICROCONTROLLER
-	if (first_t)
-	{				
-		while ( !connectAttempt(alpha->ssid_wifi, alpha->pass_wifi) )    *//* Attempt to connect to the network via WiFi, in RaspberryPi only check connection to the network. *//**//*
-		{
-			 led_blinks(0, 1, 600000);	// Blink in green GREEN - ERROR 0 (No WiFi connection);
-			 led_blinks(1, 1, 600000);	// Blink in green RED - ERROR 0 (No WiFi connection);
-		}
-	}
-	endif
-	if ( !init_socket(alpha->address, alpha->address_port,first_t) )     *//* Check Endpoint *//*
-	{	
-		udelay_basics ( 100000 );
-		led_blinks(1, 3, 70000);	// Blink in green RED - ERROR 1 (Bad connection with the endpoint);
-		#ifdef MICROCONTROLLER
-			restart_basic();
-		#endif
-	}
-}*/
 
 void pnp_sensors()
 {
     init_bme280(false);
-    
+
 }
 
 
@@ -85,16 +71,16 @@ void getData(struct device *alpha, long *count)
 {
     int i;
     ++(*count);
-	
+
     #ifdef SHELLPRINT	// Printf in shell
 	d_collect_msg( count );
 	print_sensors_state();
-    #endif  
+    #endif
 
-	
+
     /* GET DATA INTERNAL TEMPERATURE */
     strcpy(alpha->d[0], get_internal());
-	
+
     /* GET DATA BME280 */
     if (check_bme280())
     {
@@ -107,7 +93,7 @@ void getData(struct device *alpha, long *count)
 			strcpy(alpha->d[i+1], "0");
     }
 
-   
+
 }
 
 
@@ -133,7 +119,7 @@ void generateJson(struct device *alpha)
 	    beta++;
     }
     strcat(alpha->json, "]}");
-	
+
     if (check_bme280())
     {
 		beta = 0;
@@ -154,26 +140,25 @@ void generateJson(struct device *alpha)
 		}
 		strcat(alpha->json, "]}");
     }
-		 
+
 	strcat(alpha->json, "],\"device\": \"");
 	strcat(alpha->json, alpha->id);
 	strcat(alpha->json, "\",\"timestamp\": \"0\"}");
-	
-    #ifdef SHELLPRINT	
+
+    #ifdef SHELLPRINT
     	print_json(alpha->json);	// Printf in shell
     #endif
 }
 
-void t_delay(long desiredDelay, long actualDelay) 
-{  
+
+void t_delay(long desiredDelay, long actualDelay)
+{
 	if (actualDelay >= desiredDelay)		/* To prevent crashes */
 		actualDelay = desiredDelay;
-    udelay_basics ( (desiredDelay - actualDelay) * 1000000 );	/* Time set by user  minus  loss time by operation */ 
+    udelay_basics ( (desiredDelay - actualDelay) * 1000000 );	/* Time set by user  minus  loss time by operation */
 }
 
-long take_time() 
-{  
+long take_time()
+{
     return take_time_basics();
 }
-
-
--- core/lib.h
+++ core/lib.h
@@ -1,31 +1,17 @@
 #ifndef _LIB_
 #define _LIB_
-#include <stdio.h>
-#include <stdbool.h>
 #include "lib.c"
 #include "struct-device.h"
 
 void led_blinks (int, int, int);
-void config(struct device *alpha);
-void initPeripherals(long *); 
+void config(struct device *);
+void connectNetwork(struct device *, bool);
+void initPeripherals(long *);
 void pnp_sensors();
 void getData(struct device *,long *);
 void generateJson(struct device *);
+
 void t_delay(long, long);
 long take_time();
-bool init_socket(const char*, int,bool);
-void init_LEDs();
-void led_GPIO(int, int);
-bool read_GPIO(int);
-void config(struct device *alpha);
-void welcome_msg();
-void d_collect_msg(long*);
-void print_json(char*);
-void udelay_basics (long);
-long take_time_basics();
-void print_sensors_state(void);
-void init_internal(bool);
-char* get_internal();
 
 #endif
-
