20305cfc8fe4693ac7eca712906d314b5c909785
shikharaa <shikhara1997@gmail.com>
2023-05-01 19:06:42 -0400

    m

--- core/lib.c
+++ core/lib.c
@@ -6,6 +6,12 @@
 #include "config.h"
 #include "devices.h"
 
+void init_LEDs();
+void led_GPIO(int, int);
+bool read_GPIO(int);
+
+
+
 void config(struct device *alpha)
 {   
     /* User assignments */
