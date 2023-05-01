c2dca90f787679ea8114a9ca8a7fdcba52c0fc65
shikharaa <shikhara1997@gmail.com>
2023-05-01 18:48:35 -0400

    m

--- core/lib.h
+++ core/lib.h
@@ -4,7 +4,7 @@
 #include "struct-device.h"
 
 void led_blinks (int, int, int);
-void config(struct device *);
+void config(struct device *alpha);
 void connectNetwork(struct device *, bool);
 void initPeripherals(long *); 
 void pnp_sensors();
@@ -12,5 +12,5 @@ void getData(struct device *,long *);
 void generateJson(struct device *);
 void t_delay(long, long); 
 long take_time();
-
+bool init_socket(const char*, int,bool);
 #endif
