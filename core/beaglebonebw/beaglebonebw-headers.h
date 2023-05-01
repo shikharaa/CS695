63575cf0b709353d9bdaa272a7adfc52a54d35e4
shikharaa <shikhara1997@gmail.com>
2023-05-01 19:12:29 -0400

    m

--- core/beaglebonebw/beaglebonebw-headers.h
+++ core/beaglebonebw/beaglebonebw-headers.h
@@ -1,13 +1,9 @@
-/*#ifndef RASPI_3_4_HEADERS_H
-#define RASPI_3_4_HEADERS_H
+#ifndef HEADERS_H
+#define HEADERS_H
 
 #include "basics.h"
 #include "peripherals/gpio/gpio.h"
 #include "sensors/internal/internal.h"
 #include "sensors/bme280/bme280.h"
 
-
-#ifdef MICROCONTROLLER
-	#include "wifi/wifi.h"
 #endif
-#endif*/
